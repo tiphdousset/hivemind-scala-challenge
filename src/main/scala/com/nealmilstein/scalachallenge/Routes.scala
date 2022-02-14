2508f5e0fd633f96fef66db4162219c150586fab
Neal Milstein <neal.milstein@gmail.com>
2022-02-14 15:47:26 +0100

    implement POST amazon/best-rated route

--- src/main/scala/com/nealmilstein/scalachallenge/Routes.scala
+++ src/main/scala/com/nealmilstein/scalachallenge/Routes.scala
@@ -2,10 +2,14 @@ package com.nealmilstein.scalachallenge
 
 import cats.effect.Sync
 import cats.implicits._
+
 import org.http4s.HttpRoutes
 import org.http4s.dsl.Http4sDsl
 import org.http4s.circe.CirceEntityCodec._
+
+import doobie.implicits._
 import doobie.util.transactor
+
 import io.circe.generic.JsonCodec
 
 @JsonCodec
@@ -18,12 +22,24 @@ final case class BestRatedRequest(
 
 object Routes {
   def reviewRoutes[F[_]: Sync](xa: transactor.Transactor[F]): HttpRoutes[F] = {
+    def reverseDate(date: String) = date.split('.').reverse.mkString(".")
+
     val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] { case req @ POST -> Root / "amazon" / "best-rated" =>
       for {
-        bestRatedRequest <- req.as[BestRatedRequest]
-        resp <- Ok(bestRatedRequest)
+        requestJson <- req.as[BestRatedRequest]
+        bestRatings <- sql"""
+          SELECT asin, AVG(overall)
+          FROM reviews
+          WHERE created_at > CAST(${reverseDate(requestJson.start)} AS TIMESTAMP)
+            AND created_at < CAST(${reverseDate(requestJson.end)} AS TIMESTAMP)
+          GROUP BY asin
+          HAVING COUNT(id) >= ${requestJson.min_number_reviews}
+          ORDER BY AVG DESC
+          LIMIT ${requestJson.limit}
+        """.query[(String, Float)].to[List].transact(xa)
+        resp <- Ok(bestRatings)
       } yield resp
     }
   }
