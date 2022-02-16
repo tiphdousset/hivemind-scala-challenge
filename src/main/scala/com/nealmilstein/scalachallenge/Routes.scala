4bfe94b103362b538560ccfd142f99eb5c9a427f
Neal Milstein <neal.milstein@gmail.com>
2022-02-16 22:42:52 +0100

    Use better validation scheme

--- src/main/scala/com/nealmilstein/scalachallenge/Routes.scala
+++ src/main/scala/com/nealmilstein/scalachallenge/Routes.scala
@@ -10,15 +10,52 @@ import org.http4s.circe.CirceEntityCodec._
 import doobie.implicits._
 import doobie.util.transactor
 
+import io.circe._
 import io.circe.generic.JsonCodec
 
-@JsonCodec
+import java.util.Date
+import java.text.{SimpleDateFormat, ParseException}
+
 final case class BestRatedRequest(
-    start: String,
-    end: String,
+    start: Date,
+    end: Date,
     limit: Int,
     min_number_reviews: Int
 )
+
+object BestRatedRequest {
+  private implicit val dateDecoder: Decoder[Date] = new Decoder[Date] {
+    val dateFormatter = new SimpleDateFormat("dd.MM.yyyy")
+    dateFormatter.setLenient(false)
+
+    def apply(c: HCursor): Decoder.Result[Date] =
+      for {
+        dateStr <- c.value.as[String]
+        date <-
+          try {
+            Right(dateFormatter.parse(dateStr))
+          } catch {
+            case _: ParseException =>
+              Left(
+                DecodingFailure(
+                  "Attempt to decode invalid date object",
+                  c.history
+                )
+              )
+          }
+      } yield date
+  }
+
+  implicit val bestRatedRequestDecoder: Decoder[BestRatedRequest] =
+    new Decoder[BestRatedRequest] {
+      def apply(c: HCursor): Decoder.Result[BestRatedRequest] = for {
+        start <- c.downField("start").as[Date]
+        end <- c.downField("end").as[Date]
+        limit <- c.downField("limit").as[Int]
+        min_number_reviews <- c.downField("min_number_reviews").as[Int]
+      } yield BestRatedRequest(start, end, limit, min_number_reviews)
+    }
+}
 
 @JsonCodec
 final case class ProductRatingAverage(
@@ -28,32 +65,25 @@ final case class ProductRatingAverage(
 
 object Routes {
   def reviewRoutes[F[_]: Sync](xa: transactor.Transactor[F]): HttpRoutes[F] = {
-    def reverseDate(date: String) = date.split('.').reverse.mkString(".")
-    val dateRegex = raw"\d{2}\.\d{2}\.\d{4}"
-
     val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] { case req @ POST -> Root / "amazon" / "best-rated" =>
       for {
         decoded <- req.attemptAs[BestRatedRequest].value
         resp <- decoded match {
-          case Left(_) => BadRequest()
-          case Right(json)
-              if !json.start.matches(dateRegex)
-                || !json.end.matches(dateRegex) =>
-            BadRequest()
-          case Right(requestJson) =>
+          case Left(err) => BadRequest(err.message)
+          case Right(bestRatedRequest) =>
             for {
               bestRatings <- sql"""
-                  SELECT asin, AVG(overall)
-                  FROM reviews
-                  WHERE created_at > CAST(${reverseDate(requestJson.start)} AS TIMESTAMP)
-                    AND created_at < CAST(${reverseDate(requestJson.end)} AS TIMESTAMP)
-                  GROUP BY asin
-                  HAVING COUNT(id) >= ${requestJson.min_number_reviews}
-                  ORDER BY AVG DESC
-                  LIMIT ${requestJson.limit}
-                """.query[(String, Float)].to[List].transact(xa)
+              SELECT asin, AVG(overall)
+              FROM reviews
+              WHERE created_at > CAST(${bestRatedRequest.start} AS TIMESTAMP)
+                AND created_at < CAST(${bestRatedRequest.end} AS TIMESTAMP)
+              GROUP BY asin
+              HAVING COUNT(id) >= ${bestRatedRequest.min_number_reviews}
+              ORDER BY AVG DESC
+              LIMIT ${bestRatedRequest.limit}
+            """.query[(String, Float)].to[List].transact(xa)
               resp <- Ok(
                 bestRatings.map(rating =>
                   ProductRatingAverage(rating._1, rating._2)
